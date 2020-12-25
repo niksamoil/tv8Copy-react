eb3ce08d1a8fd70219dcfae7e4eace8faac1a204
Nikolay <samoil.nikolai@gmail.com>
2020-12-25 19:33:46 +0200

    prefinal

--- src/components/PostPage/PostPage.js
+++ src/components/PostPage/PostPage.js
@@ -3,36 +3,41 @@ import { Link, useParams  } from 'react-router-dom';
 import './PostPage.css';
 
 function PostPage(props) {
-    const [page, setPage] = useState();
     const [isLoaded, setIsLoaded] = useState(false);
+    const [data, setData] = useState({});
+    const [mediaId, setMediaId] = useState();
 
-    // const { match: {params : {id}}} = props;
-    let {id} = useParams();
+    const { match: {params : {id}}} = props;
 
     useEffect(() => {
 
-        fetch(`https://tv8.md/wp-json/wp/v2/posts/${id}`)
+        const getPostInfo = fetch(`https://tv8.md/wp-json/wp/v2/posts/${id}`)
         .then(res => res.json())
-        .then(
-            (result) => {
-                setIsLoaded(true);
-                setPage(result);
-                console.log(result);
+        .then((result) => result);
+        console.log(getPostInfo);
 
-            },
-            (error) => {
-                setIsLoaded(true);
-                console.log(error);
-            }
-        );
+        const getMediaInfo = fetch(`https://tv8.md/wp-json/wp/v2/media/${mediaId}`)
+        .then(res => res.json())
+        .then((result) => result);
+
+
+        Promise.all([getPostInfo, getMediaInfo]).then(res => {
+            setMediaId(res[0].featured_media);
+            setData({
+                title: res[0].title.rendered,
+                excerpt: res[0].excerpt.rendered,
+                content: res[0].content.rendered,
+                imgSrc: res[1].source_url
+            });
+            setIsLoaded(true);
+        });
 
         return () => {
             console.log('clear');
         };
-    }, [id]);
+    }, [id, mediaId]);
 
-
-    console.log(page);
+    console.log(data);
  
     if(isLoaded) {
         return (
@@ -41,9 +46,12 @@ function PostPage(props) {
                     Go back
                 </Link>
                 <h1>
-                    {page.title.rendered}
+                    {data.title}
                 </h1>
-                <div></div>
+                <div>
+                    <img className='featured-image' src={data.imgSrc} alt={data.title}/>
+                </div>
+                <div className='content' dangerouslySetInnerHTML={{ __html: data.content }}></div>
             </>
         );
     }
